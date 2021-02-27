2a80becb21d3bbc5274c9d1a41262ee6912b1c1d
Éric Périé <eperie-github@videotron.ca>
2021-02-27 14:57:17 -0500

    update to 1.6.0 with YAML suppport

--- dtc/build-dtc-mingw64.sh
+++ dtc/build-dtc-mingw64.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
-CROSS_COMPILE=/usr/bin/x86_64-w64-mingw32-
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $BASH_SOURCE)
+
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=$(dirname $SCRIPT)
+# Copyright (c) 2017-2021, Éric Périé
+# 
+#  SPDX-License-Identifier: BSD-3-Clause
+# 
+# Script for building a mingw64 version of sunxi-tools on Ubuntu 16.04.
+#
+
+# terminate on error
 set -e
 
-DTC_VERSION=1.4.7
-git clone https://git.kernel.org/pub/scm/utils/dtc/dtc.git
-cp mingw-compat.c dtc
-pushd dtc
-git checkout v${DTC_VERSION}
-make clean
-make dtc-lexer.lex.c  dtc-parser.tab.c
+CROSS_COMPILE=/usr/bin/x86_64-w64-mingw32-
 
-${CROSS_COMPILE}gcc -D__USE_MINGW_ANSI_STDIO=1 -Dlstat=mingw_lstat -I. -Ilibfdt -o dtc.exe dtc.c  checks.c  data.c dtc-lexer.lex.c dtc-parser.tab.c  livetree.c srcpos.c treesource.c util.c flattree.c fstree.c mingw-compat.c libfdt/*.c
-${CROSS_COMPILE}gcc -Ilibfdt -o fdtdump.exe fdtdump.c util.c mingw-compat.c libfdt/*.c
-${CROSS_COMPILE}gcc -Ilibfdt -o fdtoverlay.exe fdtoverlay.c util.c libfdt/*.c
-${CROSS_COMPILE}gcc -Ilibfdt -o fdtget.exe fdtget.c util.c libfdt/*.c
-${CROSS_COMPILE}gcc -Ilibfdt -o fdtput.exe fdtput.c util.c libfdt/*.c
-#${CROSS_COMPILE}gcc -Ilibfdt -o convert-dtsv0.exe convert-dtsv0-lexer.lex.c util.c libfdt/*.c -lshlwapi
+DTC_VERSION=1.6.0
+LIBYAML_VERSION=0.2.5
+SYSROOT=${SCRIPTPATH}/sysroot
 
-popd
+do_retrieve_libyaml()
+{
+  if  [ ! -f libyaml-${LIBYAML_VERSION}.tar.gz ]
+  then
+    wget http://pyyaml.org/download/libyaml/yaml-${LIBYAML_VERSION}.tar.gz
+  fi
+}
+
+do_build_libyaml()
+{
+
+  rm -rf ${SCRIPTPATH}/yaml-${LIBYAML_VERSION}
+  tar zxf ${SCRIPTPATH}/yaml-${LIBYAML_VERSION}.tar.gz -C ${SCRIPTPATH}
+
+  pushd ${SCRIPTPATH}/yaml-${LIBYAML_VERSION}
+  CROSS_COMPILE=x86_64-w64-mingw32- ./configure --host=x86_64-w64-mingw32 --prefix=${SCRIPTPATH}/sysroot
+  CROSS_COMPILE=x86_64-w64-mingw32- make all install
+  popd
+}
+
+do_retrieve_dtc()
+{
+  if [ ! -f dtc-${DTC_VERSION}.tar.gz ]
+  then
+    wget https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/dtc-${DTC_VERSION}.tar.gz
+  fi
+}
+
+do_prepare()
+{
+  rm -rf dtc-${DTC_VERSION}
+  tar zxf dtc-${DTC_VERSION}.tar.gz
+  cp mingw-compat.? dtc-${DTC_VERSION}
+  pushd dtc-${DTC_VERSION}
+  patch -p1 < ../fnmatch.diff
+  make clean
+  make dtc-lexer.lex.c  dtc-parser.tab.c
+  popd
+}
+
+do_build_dtc()
+  {
+  pushd dtc-${DTC_VERSION}
+ ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -D__USE_MINGW_ANSI_STDIO=1 -Dlstat=mingw_lstat -I. -Ilibfdt -o dtc.exe \
+      dtc.c  checks.c  data.c dtc-lexer.lex.c dtc-parser.tab.c  livetree.c srcpos.c treesource.c util.c flattree.c fstree.c mingw-compat.c yamltree.c libfdt/*.c -lyaml
+  ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -Ilibfdt -o fdtdump.exe fdtdump.c util.c mingw-compat.c libfdt/*.c -lyaml
+  ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -Ilibfdt -o fdtoverlay.exe fdtoverlay.c util.c libfdt/*.c -lyaml
+  ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -Ilibfdt -o fdtget.exe fdtget.c util.c libfdt/*.c -lyaml
+  ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -Ilibfdt -o fdtput.exe fdtput.c util.c libfdt/*.c -lyaml
+  ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML  -Ilibfdt -o convert-dtsv0.exe convert-dtsv0-lexer.lex.c util.c libfdt/*.c -lshlwapi -lyaml
+  ${CROSS_COMPILE}strip *.exe
+  popd
+}
+
+# main
+#do_retrieve_libyaml
+#do_build_libyaml
+#do_retrieve_dtc
+do_prepare
+do_build_dtc
