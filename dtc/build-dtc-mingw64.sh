b4b16a2960e978f4c401ad47b12f0db3028a3d8a
eperie <7552532+eperie@users.noreply.github.com>
2022-12-30 12:03:52 -0500

    Update build-dtc-mingw64.sh
    Build, package v1.6.1

--- dtc/build-dtc-mingw64.sh
+++ dtc/build-dtc-mingw64.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-
-# Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $BASH_SOURCE)
-
-# Absolute path this script is in. /home/user/bin
-SCRIPTPATH=$(dirname $SCRIPT)
-# Copyright (c) 2017-2021, Éric Périé
+# Copyright (c) 2017-2022, Éric Périé
 # 
 #  SPDX-License-Identifier: BSD-3-Clause
 # 
 # Script for building a mingw64 version of sunxi-tools on Ubuntu 16.04.
 #
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $BASH_SOURCE)
+
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=$(dirname $SCRIPT)
+
 # terminate on error
 set -e
 
 CROSS_COMPILE=/usr/bin/x86_64-w64-mingw32-
 
-DTC_VERSION=1.6.0
+DTC_VERSION=1.6.1
 LIBYAML_VERSION=0.2.5
 SYSROOT=${SCRIPTPATH}/sysroot
 
 do_retrieve_libyaml()
 {
-  if  [ ! -f libyaml-${LIBYAML_VERSION}.tar.gz ]
+  if  [ ! -e yaml-${LIBYAML_VERSION}.tar.gz ]
   then
     wget http://pyyaml.org/download/libyaml/yaml-${LIBYAML_VERSION}.tar.gz
   fi
@@ -75,9 +75,48 @@ do_build_dtc()
   popd
 }
 
+do_build_libfdt()
+{
+  pushd dtc-${DTC_VERSION}/libfdt
+  for FILE in *.c
+  do
+    ${CROSS_COMPILE}gcc -static -I${SYSROOT}/include -L${SYSROOT}/lib -UNO_YAML -I. -c ${FILE}
+  done
+  ${CROSS_COMPILE}ar rc libfdt.a *.o
+  popd
+}
+
+do_install()
+{
+  local TARGET=${SCRIPTPATH}/dtc-${DTC_VERSION}-x86_64-w64-mingw32
+  rm -rf ${TARGET}
+  mkdir -p ${TARGET}
+  mkdir -p ${TARGET}/lib
+  mkdir -p ${TARGET}/include
+  mkdir -p ${TARGET}/bin
+
+  pushd dtc-${DTC_VERSION}
+  cp libfdt/libfdt.a ${TARGET}/lib
+  cp libfdt/fdt.h libfdt/libfdt.h libfdt/libfdt_env.h ${TARGET}/include
+  cp dtdiff *.exe ${TARGET}/bin
+  popd
+}
+
+do_package()
+{
+  local RELEASES=${SCRIPTPATH}/releases
+  mkdir -p ${RELEASES}
+  pushd ${SCRIPTPATH}
+  tar Jcvf ${RELEASES}/dtc-${DTC_VERSION}-x86_64-w64-mingw32.tar.xz dtc-${DTC_VERSION}-x86_64-w64-mingw32
+  popd
+}
+
 # main
-#do_retrieve_libyaml
-#do_build_libyaml
-#do_retrieve_dtc
+do_retrieve_libyaml
+do_build_libyaml
+do_retrieve_dtc
 do_prepare
 do_build_dtc
+do_build_libfdt
+do_install
+do_package
